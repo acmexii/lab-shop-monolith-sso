0f78a1db61278723ffe855db9ecd162e63a0a27d
박용주 <apexacme@gmail.com>
2022-11-09 11:06:34 +0900

    commit

---
+++ monolith/src/main/java/labshopmonolithsso/domain/OrderPlaced.java
@@ -0,0 +1,23 @@
+package labshopmonolithsso.domain;
+
+import labshopmonolithsso.domain.*;
+import labshopmonolithsso.infra.AbstractEvent;
+import java.util.*;
+import lombok.*;
+
+@Data
+@ToString
+public class OrderPlaced extends AbstractEvent {
+
+    private Long id;
+    private String productId;
+    private Integer qty;
+    private String customerId;
+
+    public OrderPlaced(Order aggregate){
+        super(aggregate);
+    }
+    public OrderPlaced(){
+        super();
+    }
+}
---
+++ monolith/src/test/java/labshopmonolithsso/common/CucumberSpingConfiguration.java
@@ -0,0 +1,12 @@
+package labshopmonolithsso.common;
+
+
+import labshopmonolithsso.MonolithApplication;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@CucumberContextConfiguration
+@SpringBootTest(classes = { MonolithApplication.class })
+public class CucumberSpingConfiguration {
+    
+}
